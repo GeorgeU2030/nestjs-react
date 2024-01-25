0fbc0e233bde37d381abae2952a15dd0e247f98f
GeorgeU2030 <georgeu2021@gmail.com>
2024-01-25 17:11:46 -0500

    react_setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="h-screen bg-zinc-900 text-white flex justify-center items-center">
+      <div className="bg-gray-950 p-4 w-2/5">
+        <h1 className="text-3xl font-bold text-center block my-2">Task App</h1>
+       
+      </div>
+    </div>
+  )
+}
+
+export default App
