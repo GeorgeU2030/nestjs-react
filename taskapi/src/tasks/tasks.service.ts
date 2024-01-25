29e297f91f289bf45e557384b4dc2bbb179fe752
GeorgeU2030 <georgeu2021@gmail.com>
2024-01-25 12:05:14 -0500

    validations_and_module_config

--- taskapi/src/tasks/tasks.service.ts
+++ taskapi/src/tasks/tasks.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Task } from '../schemas/task.schema';
 import { Model } from 'mongoose';
+import { createTaskDTO } from 'src/dto/create-task.dto';
+import { updateTaskDTO } from 'src/dto/update-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,7 +13,7 @@ export class TasksService {
         this.taskModel.find();
     }
 
-    async create(createTask:any){
+    async create(createTask:createTaskDTO){
         const newTask = new this.taskModel(createTask);
         return newTask.save()
     }
@@ -24,7 +26,7 @@ export class TasksService {
         return this.taskModel.findByIdAndDelete(id)
     }
 
-    async update(id:string, updateTask:any){
+    async update(id:string, updateTask:updateTaskDTO){
         return this.taskModel.findByIdAndUpdate(id, updateTask)
     }
 }
