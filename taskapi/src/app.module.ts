9e2f5de69da2b46f56312a5d0ddc5850171df37b
GeorgeU2030 <georgeu2021@gmail.com>
2024-02-15 08:07:35 -0500

    config_to_deploy

--- taskapi/src/app.module.ts
+++ taskapi/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://georgeu2021:ferari50@cluster0.ydnvsrm.mongodb.net/taskdb?retryWrites=true&w=majority'),
-    TasksModule],
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    TasksModule,],
 })
 export class AppModule {}
